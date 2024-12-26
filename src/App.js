74cc06679a3dca3bda6da6d410582d9fd9832670
washington nieto <washington_nieto@hotmail.com>
2024-12-26 03:18:33 -0500

    props testimonios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,14 @@ function App() {
     <div className='App'>
       <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        <Testimonio 
+          nombre=''
+          pais=''
+          imagen=''
+          cargo=''
+          empresa=''
+          testimonio=''
+        />
       </div>
     </div>
   );
